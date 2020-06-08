e5c06bb7d6dc99b5dcdac62f0b236f5026913a35
Viktor Podzigun <viktor.podzigun@gmail.com>
2020-06-08 15:30:24 +0200

    Added calcite query 2

--- src/main/scala/scalcite/example/Query2.scala
+++ src/main/scala/scalcite/example/Query2.scala
@@ -2,22 +2,41 @@ package scalcite.example
 
 import java.sql.Statement
 
+import org.apache.calcite.rel.core.JoinRelType
+import org.apache.calcite.sql.fun.SqlStdOperatorTable
 import org.apache.calcite.tools.RelBuilder
 import scalcite.example.db.PostgresDatabase
 
 class Query2(db: PostgresDatabase) extends BaseQuery(db) {
 
   protected def run(statement: Statement, builder: RelBuilder): String = {
-    val resultSet = statement.executeQuery(
-      """
-        |select t.month_of_year, t.the_month, sum(f.unit_sales) as sum from sales_fact_1998 f, customer c, time_by_day t
-        |  where
-        |    f.customer_id = c.customer_id and
-        |    f.time_id = t.time_id and
-        |    c.city = 'Albany'
-        |  group by t.month_of_year, t.the_month
-        |  order by t.month_of_year;
-      """.stripMargin)
+    val node = builder
+      .scan("sales_fact_1998")
+      .scan("customer")
+      .join(JoinRelType.INNER, "customer_id")
+      .scan("time_by_day")
+      .join(JoinRelType.INNER, "time_id")
+      .filter(
+        builder.call(
+          SqlStdOperatorTable.EQUALS,
+          builder.field("city"),
+          builder.literal("Albany")
+        )
+      )
+      .aggregate(
+        builder.groupKey("month_of_year", "the_month"),
+        builder.sum(false, "sum", builder.field("unit_sales"))
+      )
+      .project(
+        builder.field("month_of_year"),
+        builder.field("the_month"),
+        builder.field("sum")
+      )
+      .sort(builder.field("month_of_year"))
+      .build
+
+    val sql = toSql(node)
+    val resultSet = statement.executeQuery(sql)
 
     val results = new StringBuilder
     while (resultSet.next()) {
